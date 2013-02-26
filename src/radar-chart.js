c7e57ce71cbd2c7bc0b0f9463b5ed1965ae38ad0
alangrafu <alvaro@graves.cl>
2013-02-26 01:34:24 -0800

    supporting multiple series of data. Changed name "variable" to "axis".
    known bug: if each series has different axis, no idea what may happen
    
    Added minified version of radar-chart-d3

--- src/radar-chart.js
+++ src/radar-chart.js
@@ -1,70 +1,98 @@
 var RadarChart = {
   draw: function(id, d, options){
     var cfg = {
-       w :600, 
-       h: 600, 
-       factor: 1, 
-       factorLegend:.85,
-       total: 4,
-       levels: 4,
-       maxValue: 1000,
-       radians: 2 * Math.PI, 
-       minDistance : 50,
+     radius: 7,
+     w :600, 
+     h: 600, 
+     factor: 1, 
+     factorLegend:.85,
+     total: 4,
+     levels: 4,
+     maxValue: 1000,
+     radians: 2 * Math.PI, 
+     minDistance : 50,
    }
    if(options != undefined){
-      for(var i in options){
-        cfg[i] = options[i];
-      }
+    for(var i in options){
+      cfg[i] = options[i];
     }
-    total = d.length;
-    cfg.maxValue = (Math.max.apply(Math,d.map(function(o){return o.value;})));
-    var radius = cfg.factor*Math.min(cfg.w/2, cfg.h/2);
-    var g = d3.select(id).append("svg").attr("width", cfg.w).attr("height", cfg.h).append("g");
+  }
+  cfg.maxValue = d3.max(d, function(i){return Math.max.apply(Math,i.map(function(o){return o.value;}))});
+  var allAxis = (d[0].map(function(i, j){return i.axis}));
+  total = allAxis.length;
+  var radius = cfg.factor*Math.min(cfg.w/2, cfg.h/2);
+  var g = d3.select(id).append("svg").attr("width", cfg.w).attr("height", cfg.h).append("g");
 
-    var axis = g.selectAll(".axis").data(d).enter().append("g");
-    axis.selectAll(".axis").data(d).enter().append("svg:line")
-    .attr("x1", cfg.w/2)
-    .attr("y1", cfg.h/2)
-    .attr("x2", function(d, i){return cfg.w/2*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
-    .attr("y2", function(d, i){return cfg.h/2*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
-    .attr("class", "line");
-    axis.selectAll(".axis").data(d).enter().append("text").attr("class", "legend")
-    .text(function(d){return d.variable})
-    .attr("x", function(d, i){return cfg.w/2*(1-cfg.factorLegend*Math.sin(i*cfg.radians/total))-20*Math.sin(i*cfg.radians/total);})
-    .attr("y", function(d, i){return cfg.h/2*(1-cfg.factorLegend*Math.cos(i*cfg.radians/total))-20*Math.cos(i*cfg.radians/total);});
-
-    for(var j=0; j<cfg.levels; j++){
-     var levelFactor = cfg.factor*radius*((j+1)/cfg.levels);
-     g.selectAll(".levels").data(d).enter().append("svg:line")
-     .attr("x1", function(d, i){return levelFactor*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
-     .attr("y1", function(d, i){return levelFactor*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
-     .attr("x2", function(d, i){return levelFactor*(1-cfg.factor*Math.sin((i+1)*cfg.radians/total));})
-     .attr("y2", function(d, i){return levelFactor*(1-cfg.factor*Math.cos((i+1)*cfg.radians/total));})
-     .attr("class", "line").attr("transform", "translate(" + (cfg.w/2-levelFactor) + ", " + (cfg.h/2-levelFactor) + ")");;
+  for(var j=0; j<cfg.levels; j++){
+   var levelFactor = cfg.factor*radius*((j+1)/cfg.levels);
+   g.selectAll(".levels").data(allAxis).enter().append("svg:line")
+   .attr("x1", function(d, i){return levelFactor*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
+   .attr("y1", function(d, i){return levelFactor*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
+   .attr("x2", function(d, i){return levelFactor*(1-cfg.factor*Math.sin((i+1)*cfg.radians/total));})
+   .attr("y2", function(d, i){return levelFactor*(1-cfg.factor*Math.cos((i+1)*cfg.radians/total));})
+   .attr("class", "line").style("stroke", "grey").style("stroke-width", "0.5px").attr("transform", "translate(" + (cfg.w/2-levelFactor) + ", " + (cfg.h/2-levelFactor) + ")");;
 
  }
 
- var testData = [
+ var color = d3.scale.category10();
+ series = 0;
+ for(x in d){
+  dataValues = [];
+  y = d[x];
+  d3.select(id+" g").selectAll(".nodes")
+  .data(y, function(j, i){
+    dataValues.push([
+      cfg.w/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total)), 
+      cfg.h/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total))
+      ]);
+  });
+  
+  dataValues.push(dataValues[0]);
+  d3.select(id+" g").selectAll(".area")
+  .data([dataValues])
+  .enter().append("path")
+  .style("fill", function(j, i){return color(series)}).style("fill-opacity", .5)
+  .attr("d", d3.svg.area());
+  series++;
+}
+series=0;
+for(x in d){
+  y = d[x];
 
- ];
- g.selectAll(".nodes").data(d).enter().append("svg:circle").attr('r', 10).attr("alt", function(d){return d.value})
- .attr("cx", function(d, i){
-    testData.push([
-       cfg.w/2*(1-(d.value/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total)), 
-       cfg.h/2*(1-(d.value/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total))
-       ]);
-    return cfg.w/2*(1-(d.value/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total));})
- .attr("cy", function(d, i){return cfg.h/2*(1-(d.value/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total));})
-                                    .attr("class", "marker").attr("data-id", function(d){return d.variable});//.attr("transform", "translate(" + (cfg.w/2) + ", " + (cfg.h/2) + ")");;
 
-                                    testData.push(testData[0]);
+  d3.select(id+" g").selectAll(".nodes")
+  .data(y).enter()
+  .append("svg:circle")
+  .attr('r', cfg.radius)
+  .attr("alt", function(j){return Math.max(j.value, 0)})
+  .attr("cx", function(j, i){
+    dataValues.push([
+      cfg.w/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total)), 
+      cfg.h/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total))
+      ]);
+    return cfg.w/2*(1-(Math.max(j.value, 0)/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total));
+  })
+  .attr("cy", function(j, i){
+    return cfg.h/2*(1-(Math.max(j.value, 0)/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total));
+  })
+  .attr("data-id", function(j){return j.axis})
+  .style("fill", color(series)).style("fill-opacity", .9)
+  .style("stroke", "black")    .append("svg:title").text(function(j){return Math.max(j.value, 0)})
+  series++;
+}
+var axis = g.selectAll(".axis").data(allAxis).enter().append("g").attr("class", "axis");
 
+axis.append("line")
+.attr("x1", cfg.w/2)
+.attr("y1", cfg.h/2)
+.attr("x2", function(j, i){return cfg.w/2*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
+.attr("y2", function(j, i){return cfg.h/2*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
+.attr("class", "line").style("stroke", "grey").style("stroke-width", "1px");
 
-                                    g.selectAll("path.area")
-    .data([testData])      // dimension of data should be 3D
-    .enter().append("path")
-    .style("fill", "steelblue").style("fill-opacity", .5)
-    .attr("class", "area") // not the cause of your problem
-    .attr("d", d3.svg.area());
+axis.append("text").attr("class", "legend")
+.text(function(d){return d}).style("font-family", "sans-serif").style("font-size", "12px").attr("transform", function(d, i){return "translate(0, -10)"})
+.attr("x", function(d, i){return cfg.w/2*(1-cfg.factorLegend*Math.sin(i*cfg.radians/total))-20*Math.sin(i*cfg.radians/total);})
+.attr("y", function(d, i){return cfg.h/2*(1-cfg.factorLegend*Math.cos(i*cfg.radians/total))-20*Math.cos(i*cfg.radians/total);});
+
 }
 }
